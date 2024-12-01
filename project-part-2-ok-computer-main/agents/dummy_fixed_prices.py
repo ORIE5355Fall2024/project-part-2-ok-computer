c1231cc67e7f265951df1a9a1d4ee45b649650cc
Man Vilailuck <55145251+ManVilailuck@users.noreply.github.com>
2024-12-01 10:29:35 -0500

    Delete project-part-2-ok-computer-main directory

--- project-part-2-ok-computer-main/agents/dummy_fixed_prices.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Agent(object):
-    def __init__(self, agent_number, params={}):
-        self.this_agent_number = agent_number  # index for this agent
-
-
-    def action(self, obs):
-        return 40
